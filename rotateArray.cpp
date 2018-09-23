83a415ad6938fbb0b0d31d1f6c249fbefb238c76
HanXu <601042507@qq.com>
2018-09-23 18:20:37 +0800

    旋转数组的最小值

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -34,7 +34,17 @@ public:
         return rotateArray[mid];
     }
 };
-
+/*排过序的数组经过旋转之后，请你找出最小的数字； [1,2,3,4,5]旋转之后位[3,4,5,1,2]
+ * 设置两个指针，head指向num[0],tail指向num[size - 1],mid=(head + tail) >> 2;
+ * 当num[mid]大于等于num[head]时，head = mid;head一直指向大数的最后一个；
+ * 当num[mid]小于等于num[tail]时，tail = mid;tail一直指向小数的第一个；
+ * 当tail - head <= 1时，就将num[tail]输出；
+ */
+ /*当数组为[0,1,1,1]时，旋转之后为[1,0,1,1]
+ * 我们会发现整个数组的num[head]=num[tail]=mun[mid]
+ * 我们只能顺序查找数组中的最小值
+ */
+ /*当数组没有旋转的时候，直接将mid设置为0，不进入循环，直接输出*/
 int main(){
     return 0;
 }
